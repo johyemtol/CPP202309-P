e8727d39e39b96b73d96f86383cf8af1d80c4adf
hyemtol <jhmlh0308@naver.com>
2023-11-21 12:04:57 +0900

    cpp 회원가입 vector로 저장

--- Sources/member.cpp
+++ Sources/member.cpp
@@ -1,13 +1,19 @@
 #include "member.h"
 
-void memberSave() {
-
+//특별한 숫자 지정해서 볼수 있게 지정함
+void memberSave(const vector<Member>& members) {
+	for (const auto& member : members) {
+		cout << "회원 이름: " << member.username << ", 회원 비밀번호: " << member.password << endl;
+	}
 }
-void memberJoin() {
+void memberJoin(vector<Member>& members) {
 	Member newMember;
 	cout << "이름: ";
 	cin >> newMember.username;
 	cout << "비밀번호: ";
 	cin >> newMember.password;
 
+	members.push_back(newMember);
+
+	cout << "회원가입이 완료되었습니다!\n";
 }
--- Sources/member.h
+++ Sources/member.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
 struct Member {
 	string username, password;
 };
 
-void memberSave();
-void memberJoin();
+void memberSave(const vector<Member>& members);
+void memberJoin(vector<Member>& members);
