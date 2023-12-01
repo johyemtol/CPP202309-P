371c0f1fc4411efe4c0b0dac39271053e19032c9
hyemtol <jhmlh0308@naver.com>
2023-12-01 16:41:48 +0900

    로그인 한 사람만 이용권 선택 가능 기능 추가

--- Sources/member.h
+++ Sources/member.h
@@ -8,6 +8,11 @@ struct Member {
 	string username, password;
 };
 
+struct UserSession {
+	string username;
+	bool login;
+};
+
 void memberSave(const vector<Member>& members);
 void memberJoin(vector<Member>& members);
-void memberLogin(const vector<Member>& members);
+void memberLogin(const vector<Member>& members, UserSession& session);
